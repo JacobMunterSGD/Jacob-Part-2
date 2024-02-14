bc2cf0a0ff3dc1cfa00d833e2bfe9c98f5e2a9cb
munter <munter@sheridancollege.ca>
2024-02-14 10:19:21 -0500

    task1b complete - button now switches between scenes

---
+++ Assets/Week 6/Scripts/SceneNameText.cs
@@ -0,0 +1,26 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SceneNameText : MonoBehaviour
+{
+
+    TextMeshProUGUI sceneName;
+
+    void Start()
+    {
+        sceneName = GetComponent<TextMeshProUGUI>();
+        sceneName.text = SceneManager.GetActiveScene().name;
+    }
+
+    void Update()
+    {
+        
+
+
+    }
+}
