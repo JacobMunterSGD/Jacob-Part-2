8bb6ec47a429fee470dc29666ec72f773579901f
Jacob Munter <munter@sheridancollege.ca>
2024-02-17 10:29:28 -0500

    The main movement script is finished (similar to the knight from the in-class assignments) You can boost the fish's speed by pressing space - this will get turned into a button later

---
+++ Assets/Assignment/Scripts/Fish.cs
@@ -0,0 +1,77 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 destination;
+    Vector2 direction;
+
+    public float moveSpeed;
+    public float minSpeed;
+    public float boostLose;
+    public float boost;
+    public float maxSpeed;
+
+    bool isClickingOnSelf = false;
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+        moveSpeed = minSpeed;
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        direction = destination - (Vector2)transform.position;
+
+        if (direction.magnitude < 0.1)
+        {
+            direction = Vector2.zero;
+            moveSpeed = minSpeed;
+        }
+
+        rb.MovePosition(rb.position + direction.normalized * Time.deltaTime * moveSpeed);
+    }
+
+    void Update()
+    {
+
+        if (moveSpeed > minSpeed)
+        {
+            moveSpeed -= Time.deltaTime * boostLose;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && moveSpeed < maxSpeed)
+        {
+            moveSpeed += boost;
+        }
+
+        if (Input.GetMouseButtonDown(0) == true && isClickingOnSelf == false)
+        {
+        
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        isClickingOnSelf = true;
+
+    }
+
+    private void OnMouseUp()
+    {
+        isClickingOnSelf = false;
+    }
+
+}
